6de1a645315e7ab711621f1949a66a61f3f5e604
Mohamed <mohamedkhaled10007@gmail.com>
2026-02-02 22:52:09 +0200

    package changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,18 @@
-// lib/db.ts
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+})
 
-export const prisma =
-    globalForPrisma.prisma || new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    adapter,
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
